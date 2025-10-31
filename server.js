fbea13eefe79ac197c4f4a1d11ce8f8fbc800c55
Naveen M <navin@example.com>
2025-10-31 13:09:55 +0530

    Clean PopChat ready for deploy

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-
 const PORT = process.env.PORT || 3000;
 const staticDir = path.join(__dirname, "src");
-
 app.use(express.static(staticDir));
 app.get("*", (req, res) => res.sendFile(path.join(staticDir, "index.html")));
-
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("User connected");
   socket.on("message", (msg) => socket.broadcast.emit("message", msg));
-  socket.on("disconnect", () => console.log("User disconnected"));
 });
-
 server.listen(PORT, () => console.log("Server running on port", PORT));
-
